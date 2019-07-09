62ab94d6aae314e7e617510286f7867ba16ce58c
h-hiroki <gyouzakuuka@gmail.com>
2019-07-09 21:22:06 +0900

    一覧画面っぽいやつを作ることができたぜー

--- template/main.js
+++ template/main.js
@@ -6,5 +6,21 @@ let app = new Vue({
       { id: 2, name: "ゴブリン", hp: 200},
       { id: 3, name: "ドラゴン", hp: 500},
     ]
+  },
+  methods: {
+    doAdd: function() {
+      // list内でもっとも大きいidを取得
+      let max = this.list.reduce(function(a, b) {
+        return a > b.id ? a : b.id
+      }, 0)
+      this.list.push({
+        id: max + 1,
+        name: this.name, // this.nameは現在のフォームの入力値
+        hp: 500
+      })
+    },
+    doRemove: function(index) {
+      this.list.splice(index, 1)
+    },
   }
 })
