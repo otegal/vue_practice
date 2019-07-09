c953818925d387b8bc69f94f426e08fcdc72387d
h-hiroki <gyouzakuuka@gmail.com>
2019-07-09 21:04:51 +0900

    ついにsvgも操作できるようになりました

--- template/main.js
+++ template/main.js
@@ -1,15 +1,6 @@
 let app = new Vue({
   el: '#app',
   data: {
-    count: 0,
-    isChild: true,
-    isActive: true,
-    textColor: 'red',
-    bgColor: 'lightgray'
-  },
-  methods: {
-    increment: function() {
-      this.count += 1
-    }
+    radius: 50
   }
 })
