bd3e3574e763459f2bb3138497300a7f84cec70f
h-hiroki <gyouzakuuka@gmail.com>
2019-07-15 00:53:14 +0900

    絞り込み機能を作成

--- template/main.js
+++ template/main.js
@@ -1,6 +1,24 @@
 let app = new Vue({
   el: '#app',
   data: {
-    val: [],
+    budget: 300,
+    limit: 2,
+    list: [
+      { id: 1, name: 'りんご', price: 100 },
+      { id: 2, name: 'ばなな', price: 200 },
+      { id: 3, name: 'いちご', price: 300 },
+      { id: 4, name: 'おれんじ', price: 400 },
+      { id: 5, name: 'めろん', price: 500 },
+    ]
   },
+  computed: {
+    matched: function() {
+      return this.list.filter(function(el) {
+        return el.price <= this.budget
+      }, this)
+    },
+    limited: function () {
+      return this.matched.slice(0, this.limit)
+    }
+  }
 })
