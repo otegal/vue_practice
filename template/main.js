b8d4e869bc47420fd92763bcb2dd35bf3619dae4
h-hiroki <gyouzakuuka@gmail.com>
2019-07-14 23:47:25 +0900

    DOMの直接変更

--- template/main.js
+++ template/main.js
@@ -1,15 +1,14 @@
 let app = new Vue({
   el: '#app',
   data: {
-    list: [
-      { id: 1, name: "スライム", hp: 100},
-      { id: 2, name: "ゴブリン", hp: 200},
-      { id: 3, name: "ドラゴン", hp: 500},
-    ]
+    show: true
   },
   methods: {
-    doAttack: function(index) {
-      this.list[index].hp -= 10
+    handleClick() {
+      let count = this.$refs.count
+      if (count) {
+        count.innerText = parseInt(count.innerText, 10) + 1
+      }
     }
   }
 })
