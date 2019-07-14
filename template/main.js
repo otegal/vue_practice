d4dbdf351bb3c2701ceff3e4e2caa0b464905706
h-hiroki <gyouzakuuka@gmail.com>
2019-07-15 01:15:46 +0900

    フィルタを独自定義する

--- template/main.js
+++ template/main.js
@@ -1,20 +1,11 @@
 let app = new Vue({
   el: '#app',
   data: {
-    list: [],
-    current: '',
-    topics: [
-      { value: 'vue', name: 'Vue.js' },
-      { value: 'jQuery', name: 'jQuery' },
-    ]
+    price: 19800,
   },
-  watch: {
-    current: function(val) {
-      axios.get('https://api.github.com/search/repositories', {
-        params: { q: 'topic:' + val }
-      }).then(function(response) {
-        this.list = response.data.items
-      }.bind(this))
+  filters: {
+    localNum: function(val) {
+      return val.toLocaleString()
     }
-  },
+  }
 })
