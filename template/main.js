0eb052640594b6611429b4dd66f737af6722f322
h-hiroki <gyouzakuuka@gmail.com>
2019-07-23 19:58:21 +0900

    子コンポーネントのイベントを親コンポーネントがキャッチできるようになったよ

--- template/main.js
+++ template/main.js
@@ -1,15 +1,18 @@
 Vue.component('comp-child', {
-  props: ['name', 'hp'], // propsで受け取るアトリビュート名を指定する
-  template: '<li>{{ name }} HP {{ hp }}</li>', // propsで受け取ったvalを利用してテンプレートを作成する。
+  // props: ['name', 'hp'],
+  template: '<button v-on:click="handleClick">イベント発火</button>',
+  methods: {
+    handleClick: function() {
+      this.$emit('childs-event')
+    }
+  }
 })
 
 new Vue({
   el: '#app',
-  data: {
-    list: [
-      { id: 1, name: 'スライム', hp: 100 },
-      { id: 2, name: 'ゴブリン', hp: 200 },
-      { id: 3, name: 'ドラゴン', hp: 300 },
-    ]
+  methods: {
+    parentsMethod: function() {
+      alert('イベントをキャッチ')
+    }
   }
 })
