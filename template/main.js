5ccd6ded8dc2bda816a9421bf02a8a24b233da4c
h-hiroki <gyouzakuuka@gmail.com>
2019-07-23 19:53:39 +0900

    親コンポーネントから子コンポーネントへpropsを渡す

--- template/main.js
+++ template/main.js
@@ -1,11 +1,15 @@
 Vue.component('comp-child', {
-  props: ['val'], // propsで受け取るアトリビュート名を指定する
-  template: '<p>{{ val }}</p>', // propsで受け取ったvalを利用してテンプレートを作成する。
+  props: ['name', 'hp'], // propsで受け取るアトリビュート名を指定する
+  template: '<li>{{ name }} HP {{ hp }}</li>', // propsで受け取ったvalを利用してテンプレートを作成する。
 })
 
 new Vue({
   el: '#app',
   data: {
-    message: 'eeesss'
+    list: [
+      { id: 1, name: 'スライム', hp: 100 },
+      { id: 2, name: 'ゴブリン', hp: 200 },
+      { id: 3, name: 'ドラゴン', hp: 300 },
+    ]
   }
 })
